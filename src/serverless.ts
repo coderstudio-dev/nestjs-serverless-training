385428e273d3ebf7575a1cacedab4ee467df1f3f
Ronald DC <rcdelacruz@gmail.com>
2023-03-13 01:00:05 +0000

    initial commit

---
+++ src/serverless.ts
@@ -0,0 +1,19 @@
+// src/serverless.ts
+
+import { configure as serverlessExpress } from '@vendia/serverless-express';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+let cachedServer;
+
+export const handler = async (event: any, context: any) => {
+  if (!cachedServer) {
+    const nestApp = await NestFactory.create(AppModule);
+    await nestApp.init();
+    cachedServer = serverlessExpress({
+      app: nestApp.getHttpAdapter().getInstance(),
+    });
+  }
+
+  return cachedServer(event, context);
+};
