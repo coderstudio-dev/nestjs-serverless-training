7483a7bded5448fda8496f13d1418cc7f16ba866
lery <jnavarro@stratpoint.com>
2023-03-23 15:07:38 +0000

    fix: import follow module

--- src/Domain/domain.module.ts
+++ src/Domain/domain.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ArticleModule } from './Article/article.module';
 import { ArticleReactionsModule } from './ArticleReaction/article-reactions.module';
 import { TagsModule } from './Tag/tags.module';
-import { FollowsModule } from 'src/follows/follows.module';
+import { FollowsModule } from './Follow/follows.module';
 
 @Module({
   imports: [ArticleModule, ArticleReactionsModule, TagsModule, FollowsModule],
