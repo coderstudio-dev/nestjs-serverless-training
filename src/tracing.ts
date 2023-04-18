0a12834f29a4d10faf6ad363b0a74defdac85ef9
lery <jnavarro@stratpoint.com>
2023-04-18 10:58:52 +0000

    build: implement honeycomb tracer for production

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,4 +1,7 @@
-import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import {
+  SimpleSpanProcessor,
+  BatchSpanProcessor,
+} from '@opentelemetry/sdk-trace-base';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import * as process from 'process';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
@@ -7,27 +10,34 @@ import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core'
 import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
-// import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
-
-// const traceExporter = new ConsoleSpanExporter();
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+// import * as dotenv from 'dotenv';
 
 const jaegerExporter = new JaegerExporter({
   // endpoint: 'http://138.2.65.251:14269/api/traces',
   endpoint: 'http://localhost:14268/api/traces',
 });
-const traceExporter = jaegerExporter;
 
-// const zipKinExporter = new ZipkinExporter({
-//   url: 'https://zipkin.coderstudio.site/',
-// });
-// const traceExporter = zipKinExporter;
+const oltpExporter = new OTLPTraceExporter({
+  url: `https://api.honeycomb.io/v1/traces`,
+  headers: {
+    'x-honeycomb-team': process.env.HONEYCOMB_API_KEY,
+  },
+});
+
+const traceExporter =
+  process.env.NODE_ENV === `development` ? jaegerExporter : oltpExporter;
+
+const spanProcessor =
+  process.env.NODE_ENV === `development`
+    ? new SimpleSpanProcessor(traceExporter)
+    : new BatchSpanProcessor(traceExporter);
 
 export const otelSDK = new NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: 'nestjs-otel', // update this to a more relevant name for you!
   }),
-  spanProcessor: new SimpleSpanProcessor(traceExporter),
-  // spanProcessor: new BatchSpanProcessor(traceExporter),
+  spanProcessor,
   instrumentations: [
     new HttpInstrumentation(),
     new ExpressInstrumentation(),
